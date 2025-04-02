5427093c3b56f1b5cee72810cad473a53c298380
Pranav-Panchal <pranavkpanchal4@gmail.com>
2025-04-01 20:41:24 -0400

    changed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
-import connectDB from "@/lib/dbConnect";
 import User from "@/models/User";
+import connectDB from "@/lib/dbConnect";
 
 const handler = NextAuth({
   providers: [
@@ -14,27 +14,13 @@ const handler = NextAuth({
       },
       async authorize(credentials) {
         await connectDB();
-
         const user = await User.findOne({ email: credentials?.email });
 
-        if (!user) {
-          throw new Error("No user found with that email.");
-        }
+        if (!user) throw new Error("No user found with that email.");
+        const isValid = await bcrypt.compare(credentials!.password, user.password);
+        if (!isValid) throw new Error("Invalid credentials");
 
-        const isValidPassword = await bcrypt.compare(
-          credentials!.password,
-          user.password
-        );
-
-        if (!isValidPassword) {
-          throw new Error("Invalid credentials");
-        }
-
-        return {
-          id: user._id.toString(),
-          name: user.name,
-          email: user.email,
-        };
+        return { id: user._id.toString(), name: user.name, email: user.email };
       },
     }),
   ],
